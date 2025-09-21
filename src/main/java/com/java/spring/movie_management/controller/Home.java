197b305c15cf3235fb8415757ab374af7707e78c
Martin Sang Do <dothanhsang1984@yahoo.com>
2025-09-21 16:36:34 +0700

    - async function

--- src/main/java/com/java/spring/movie_management/controller/Home.java
+++ src/main/java/com/java/spring/movie_management/controller/Home.java
@@ -1,15 +1,29 @@
 package com.java.spring.movie_management.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.java.spring.movie_management.service.EmployeeService;
 
 @Controller
 public class Home {
+    @Autowired
+    EmployeeService employeeService;
 
     @GetMapping("/")
     @ResponseBody
     public String showHomePage(){
         return "Hi!";
+    }
+
+    @PostMapping("/welcome")
+    public ResponseEntity<String> sendWelcome(@RequestParam String email) {
+        employeeService.sendWelcomeEmail(email);  // async
+        return ResponseEntity.accepted().body("Email is being processed");
     }
 }
--- src/main/java/com/java/spring/movie_management/service/EmployeeService.java
+++ src/main/java/com/java/spring/movie_management/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.java.spring.movie_management.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.java.spring.movie_management.model.Employee;
@@ -22,6 +23,12 @@ public class EmployeeService {
         Employee savedEmployee = employeeRepository.findEmployeeById(id);
         EmployeeDTO employeeDTO = mapper.toDto(savedEmployee);  
         return employeeDTO;
+    }
+
+    @Async
+    public void sendWelcomeEmail(String to) {
+        // simulate long task
+        System.out.println("Email sent to " + to);
     }
 
 }
