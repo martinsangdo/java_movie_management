4280d05f66d57ba1af1261e39aa46790671dc0e6
Martin Sang Do <dothanhsang1984@yahoo.com>
2025-10-01 13:32:28 +0700

    - html file to get movies

--- src/main/java/com/java/spring/movie_management/controller/MovieController.java
+++ src/main/java/com/java/spring/movie_management/controller/MovieController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -62,6 +63,7 @@ public class MovieController {
         return new ResponseEntity<>("Booking for user ID: " + userId, HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "http://localhost:8088")
     @GetMapping("/api/public/top-movies")
     public ResponseEntity<Page<Movie>> getTop10Movies(){
         Page<Movie> allMovies = movieService.findAllPagination(PageRequest.of(0, 10));
