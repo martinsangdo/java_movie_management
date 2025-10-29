496b34347fed3dbd1aee1086f8a287efe7af52fd
Martin Sang Do <dothanhsang1984@yahoo.com>
2025-10-30 05:27:45 +0700

    + Add template for 4.1

--- src/main/java/com/java/spring/movie_management/controller/AccountController.java
+++ src/main/java/com/java/spring/movie_management/controller/AccountController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +16,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.java.spring.movie_management.model.Account;
 import com.java.spring.movie_management.model.AccountDTO;
+import com.java.spring.movie_management.model.Song;
 import com.java.spring.movie_management.model.Transaction;
 import com.java.spring.movie_management.service.AccountService;
+import com.java.spring.movie_management.util.JwtUtil;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -25,6 +28,7 @@ import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
 @Controller
@@ -118,5 +122,19 @@ public class AccountController {
     public ResponseEntity<AccountDTO> createAccount(@RequestBody Account dto) {
         AccountDTO saved = accountService.createAccount(dto);
         return new ResponseEntity<>(saved, HttpStatus.CREATED);
+    }
+
+    //4.1
+    @GetMapping("/admin/dashboard")
+    public String showAdminDashboard(Model model) {
+        return "sneat/html/index";
+    }
+    //user profile page
+    @GetMapping("/profile")
+    public String showProfile(HttpSession session, Model model) {
+        String token = (String) session.getAttribute("JWT_TOKEN");
+        System.out.println(token);
+        //
+        return "sneat/html/pages-account-settings-account";
     }
 }
